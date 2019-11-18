470425d371b6e0922ed10c9c9d3811970d5cb34f
Alessandro Miguez <afmiguez@gmail.com>
2019-11-18 11:16:42 +0000

    -implemented Client controller; -created Data Test

--- src/main/java/edu/ufp/esof/order/repositories/ClientRepo.java
+++ src/main/java/edu/ufp/esof/order/repositories/ClientRepo.java
@@ -4,6 +4,9 @@ import edu.ufp.esof.order.models.Client;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ClientRepo extends CrudRepository<Client,Long> {
+    Optional<Client> findByName(String name);
 }
